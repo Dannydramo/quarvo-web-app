e3a30fcf4b44b156f207affe8145e2247a939ac7
Dannydramo <alumandudaniel217@gmail.com>
2023-10-11 10:51:12 -0700

    removed api folder for now

--- app/(auth)/signup/components/Form.tsx
+++ app/(auth)/signup/components/Form.tsx
@@ -63,27 +63,57 @@ const Form = () => {
                     <div className="">
                         <label htmlFor="firstname" className='text-sm text-white font-medium'>First Name</label>
                         <Input type="text" className={`outline-none mt-1 border focus:border-none ${inputValidity.firstName ? "border-red-500" : ""}`} onChange={(e) => handleInputChange(e, "firstName")} />
+                        {inputValidity.firstName && (
+                            <p className="text-red-500 text-sm mt-1">
+                                First Name is required.
+                            </p>
+                        )}
                     </div>
                     <div className="">
                         <label htmlFor="lastname" className='text-sm text-white font-medium'>Last Name</label>
                         <Input type="text" className={`outline-none mt-1 border focus:border-none ${inputValidity.lastName ? "border-red-500" : ""}`} onChange={(e) => handleInputChange(e, "lastName")} />
+                        {inputValidity.lastName && (
+                            <p className="text-red-500 text-sm mt-1">
+                                Last Name is required.
+                            </p>
+                        )}
                     </div>
 
                     <div className="">
                         <label htmlFor="email" className='text-sm text-white font-medium'>Email</label>
                         <Input type="email" className={`outline-none mt-1 border focus:border-none ${inputValidity.email ? "border-red-500" : ""}`} onChange={(e) => handleInputChange(e, "email")} />
+                        {inputValidity.email && (
+                            <p className="text-red-500 text-sm mt-1">
+                                Email Address is required.
+                            </p>
+                        )}
                     </div>
                     <div className="">
                         <label htmlFor="phone" className='text-sm text-white font-medium'>Phone Number</label>
-                        <Input type="tel" className={`outline-none mt-1 border focus:border-none ${inputValidity.phoneNumber ? "border-red-500" : ""}`} onChange={(e) => handleInputChange(e, "phoneNumber")} />
+                        <Input type="tel" maxLength={11} minLength={11} className={`outline-none mt-1 border focus:border-none ${inputValidity.phoneNumber ? "border-red-500" : ""}`} onChange={(e) => handleInputChange(e, "phoneNumber")} />
+                        {inputValidity.phoneNumber && (
+                            <p className="text-red-500 text-sm mt-1">
+                                Phone Number is required.
+                            </p>
+                        )}
                     </div>
                     <div className="">
                         <label htmlFor="password" className='text-sm text-white font-medium'>Password</label>
                         <Input type="password" className={`outline-none mt-1 border focus:border-none ${inputValidity.password ? "border-red-500" : ""}`} onChange={(e) => handleInputChange(e, "password")} />
+                        {inputValidity.password && (
+                            <p className="text-red-500 text-sm mt-1">
+                                Password is required.
+                            </p>
+                        )}
                     </div>
                     <div className="">
                         <label htmlFor="confirmpassword" className='text-sm text-white font-medium'>Confirm Password</label>
                         <Input type="password" className={`outline-none mt-1 border focus:border-none ${inputValidity.confirmPassword ? "border-red-500" : ""}`} onChange={(e) => handleInputChange(e, "confirmPassword")} />
+                        {inputValidity.confirmPassword && (
+                            <p className="text-red-500 text-sm mt-1">
+                                Confirm Password is required.
+                            </p>
+                        )}
                     </div>
                 </div>
 
